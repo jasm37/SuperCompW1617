bff34de20fe40f9d31817d93a50fa64e685d75b3
Yehor Yudin <ga78pal@tum.de>
2017-01-24 20:15:38 +0100

    now parser should work fine:wq

--- Assignment4/parse.py
+++ Assignment4/parse.py
@@ -3,14 +3,15 @@
 
 #args = getopt.getopt(argv,"hi:o:",["ifile="])
 
-inputfile = open('runres_collective_all');
-outputfile = open('collectall.csv','w');
+inputfile = open('io_results.out');
+outputfile = open('io_all.csv','w');
 
 problemsizes = [64,512,1024,2048,4096,8192];
 procsizes = [8,16,32,64];
 i=0;
 j=0;
 newblock = 0;
+rankcount = 0;
 
 
 for line in inputfile:
@@ -20,10 +21,13 @@ for line in inputfile:
             if j>3:
                 j=0;
                 i=i+1;
-        if line[0:5] == "[R00]":
+        #if line[0:5] == "[R00]":
+        if rankcount > procsizes[j]-2:
             newblock = 1
+            rankcount = 0;
         else:
             newblock = 0
+            rankcount=rankcount+1;
         problems=problemsizes[i]
         proc=procsizes[j]
         line=line.replace(";","")
@@ -31,5 +35,4 @@ for line in inputfile:
         line=line.replace("]","")
         times=line.split(' ')
         outputfile.write('{}, {}, {}, {}, {}, {}, {}, {}\n'.format(problems,proc,times[0],times[3],times[5],times[7],times[9],times[11]))
-        
 #outputfile.close()
